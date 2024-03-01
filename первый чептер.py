e99846777c9b2c913be1a53ea06be5f54c683bdd
AlisaVyzhimova <159880117+AlisaVyzhimova@users.noreply.github.com>
2024-03-01 20:03:08 +0300

    Create первый чептер.py

---
+++ первый чептер.py
@@ -0,0 +1,17 @@
+#задание 3
+a=input("Введите имя")
+print("Hello,",a)
+
+#4
+город=input('Введите название города:')
+время=input('введите время:')
+print("Current location is", город, 'and time is', время)
+#задание
+
+#5
+a=int(input())
+b=float(input())
+c=int(input())
+print(a)
+print(b)
+print(c)
