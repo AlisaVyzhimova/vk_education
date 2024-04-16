413ea3c4f59fad548170d573e4dc39f1cd7a1b19
AlisaVyzhimova <159880117+AlisaVyzhimova@users.noreply.github.com>
2024-04-16 09:35:25 +0300

    Update первый чептер.py

--- первый чептер.py
+++ первый чептер.py
@@ -1,17 +1,27 @@
-#задание 3
+#задание 1
 a=input("Введите имя")
 print("Hello,",a)
 
-#4
+#2
 город=input('Введите название города:')
 время=input('введите время:')
 print("Current location is", город, 'and time is', время)
 #задание
 
-#5
-a=int(input())
-b=float(input())
-c=int(input())
-print(a)
-print(b)
-print(c)
+#3
+a1 = input()
+a2 = input()
+a3 = input()
+while a3 !="":
+    a3 = input ()
+if a1 <= a3 >= a2:
+    print (True)
+else:
+    print (False)
+  #4
+def a(s):
+    return ('a' in s or 'o' in s) and ('i' not in s) and ('e' not in s)
+
+if __name__ == "__main__":
+    letter = input().strip()
+    print(a(letter))
